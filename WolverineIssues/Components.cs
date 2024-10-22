813d11fe900129182cb3e48c8ee643eee7299098
Jorik van der Werf <jorikvdwerf@gmail.com>
2024-10-22 15:19:53 +0200

    Show EventStreamUnexpectedMaxEventIdException issue

--- WolverineIssues/Components.cs
+++ WolverineIssues/Components.cs
@@ -1,3 +1,4 @@
+using Marten;
 using Marten.Events;
 using Wolverine.Attributes;
 
@@ -7,20 +8,40 @@ public class Aggregate;
 
 public sealed record MyEvent;
 
+public sealed record AnotherEvent;
+
+// 2 handler classes both listen to MyEvent. Both cascade a command that store a new event on the same stream.
+
 public static class MyFirstEventHandler
 {
-    [StickyHandler(nameof(MyFirstEventHandler))]
-    public static void Handle(IEvent<MyEvent> @event)
+    public record MyFirstCommand(Guid AggregateId);
+
+    public static MyFirstCommand Handle(IEvent<MyEvent> @event)
     {
         Console.WriteLine($"{nameof(MyFirstEventHandler)}");
+        return new MyFirstCommand(@event.StreamId);
+    }
+
+    public static async Task Handle(MyFirstCommand command, IDocumentSession session)
+    {
+        session.Events.Append(command.AggregateId, new AnotherEvent());
+        await session.SaveChangesAsync();
     }
 }
 
 public static class MySecondEventHandler
 {
-    [StickyHandler(nameof(MySecondEventHandler))]
-    public static void Handle(IEvent<MyEvent> @event)
+    public record MySecondCommand(Guid AggregateId);
+
+    public static MySecondCommand Handle(IEvent<MyEvent> @event)
     {
         Console.WriteLine($"{nameof(MySecondEventHandler)}");
+        return new MySecondCommand(@event.StreamId);
+    }
+
+    public static async Task Handle(MySecondCommand command, IDocumentSession session)
+    {
+        session.Events.Append(command.AggregateId, new AnotherEvent());
+        await session.SaveChangesAsync();
     }
 }
